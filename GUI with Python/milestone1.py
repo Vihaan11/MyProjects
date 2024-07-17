6fc4e8d5f97ecb279d6ecb15ca75c6e84733e9ec
Vihaan <vihaan111111@gmail.com>
2024-07-17 09:59:47 +0530

    more commits

--- GUI with Python/milestone1.py
+++ GUI with Python/milestone1.py
@@ -6,18 +6,45 @@ import tkinter.font as font
 dpi_awareness()
 
 class ConverterWindow(tk.Tk):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.title("Distance Converter")
-        self.main = ttk.Frame(self,padding=(30,15))
-        self.main.grid()
+        main = MetresToFeet(self,padding=(30,15))
+        self.bind('<Return>', main.calc_feet)
+        self.bind('<KP_Enter>', main.calc_feet)
+        self.bind('<Alt-f>', main.calc_feet)
+        main.grid()
 
-class MetresToFeet(object):
-    """docstring for MetresToFeet."""
 
-    def __init__(self, arg):
-        super(MetresToFeet, self).__init__()
-        self.arg = arg
+class MetresToFeet(ttk.Frame):
+    def __init__(self, container, **kwargs):
+        super().__init__(container, *kwargs)
+        self.metres_value = tk.StringVar()
+        self.feet_value=tk.StringVar(value="Start typing to see result")
+
+        metre_label = ttk.Label(self, text="Metres: ")
+        metre_input = ttk.Entry(self, width=20, textvariable=self.metres_value, font=('Comic Sans MS', 15))
+
+        feet_label = ttk.Label(self, text="Feet: ")
+        feet_display = tk.Label(self, textvariable=self.feet_value, font=('Comic Sans MS', 15))
+
+        calc_button = ttk.Button(self, text="Convert", command=calc_feet)
+
+        for child in self.winfo_children():
+            child.grid_configure(padx=15, pady=15)
+
+    def calc_feet(self, *args):
+        try:
+            global feet
+            metres = float(self.metres_value.get())
+            feet = metres * 3.281
+            # print(f"Value in feet is {feet}")
+            self.feet_value.set(f"{feet:.3f}")
+        except ValueError:
+            pass
+        except:
+            metres_value.set(0)
+
 
 
 root = ConverterWindow()
@@ -35,35 +62,6 @@ font.nametofont('TkDefaultFont').configure(size=13)
 
 # Code goes here👇
 
-metres_value = tk.StringVar()
-feet_value=tk.StringVar(value="Start typing to see result")
-
-def calc_feet(*args):
-    try:
-        global feet
-        metres = float(metres_value.get())
-        feet = metres * 3.281
-        # print(f"Value in feet is {feet}")
-        feet_value.set(f"{feet:.3f}")
-    except ValueError:
-        pass
-    except:
-        metres_value.set(0)
-
-
-
-metre_label = ttk.Label(root.main, text="Metres: ")
-metre_input = ttk.Entry(root.main, width=20, textvariable=metres_value, font=('Comic Sans MS', 15))
-# metre_input =spin_box = ttk.Spinbox(root.main,
-#                                     from_=0,
-#                                     to=999,
-#                                     textvariable=metres_value,
-#                                     wrap=True)
-
-feet_label = ttk.Label(root.main, text="Feet: ")
-feet_display = tk.Label(root.main, textvariable=feet_value, font=('Comic Sans MS', 15))
-
-calc_button = ttk.Button(root.main, text="Convert", command=calc_feet)
 
 metre_label.grid(row=0, column=0, sticky='w', padx=15, pady=15)
 metre_input.grid(row=0, column=1, sticky='EW', padx=15, pady=15)
@@ -72,13 +70,9 @@ feet_label.grid(row=1, column=0, sticky='w', padx=15, pady=15)
 feet_display.grid(row=1, column=1, padx=15, pady=15, sticky='w')
 calc_button.grid(row=2, column=0, columnspan=2, sticky='EW', padx=15, pady=15)
 
-for child in root.main.winfo_children():
-    child.grid_configure(padx=15, pady=15)
 
 
-metre_input.bind('<Return>', calc_feet)
-metre_input.bind('<KP_Enter>', calc_feet)
-metre_input.bind('<Alt-f>', calc_feet)
+
 
 # Starting the loop
 root.mainloop()
